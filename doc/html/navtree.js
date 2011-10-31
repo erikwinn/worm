77e809a044ba381ed8514d0e6c37ff41f23639e0
Erik Winn <erikwinnmail@yahoo.com>
2011-10-31 15:09:33 -0700

    Adding doxygen docs (for those w/out doxygen)

---
+++ doc/html/navtree.js
@@ -0,0 +1,353 @@
+var NAVTREE =
+[
+  [ "WORM", "index.html", [
+    [ "WORM - ORM framework", "index.html", null ],
+    [ "Modules", "modules.html", [
+      [ "WSql", "group___w_sql.html", null ]
+    ] ],
+    [ "Class List", "annotated.html", [
+      [ "WSql::WMysqlDriver", "class_w_sql_1_1_w_mysql_driver.html", null ],
+      [ "WSql::WMysqlResult", "class_w_sql_1_1_w_mysql_result.html", null ],
+      [ "WSql::WormClassGenerator", "class_w_sql_1_1_worm_class_generator.html", null ],
+      [ "WSql::WormCodeTemplate", "class_w_sql_1_1_worm_code_template.html", null ],
+      [ "WSql::WSqlColumn", "class_w_sql_1_1_w_sql_column.html", null ],
+      [ "WSql::WSqlDabataseManager", "class_w_sql_1_1_w_sql_dabatase_manager.html", null ],
+      [ "WSql::WSqlDatabase", "class_w_sql_1_1_w_sql_database.html", null ],
+      [ "WSql::WSqlDatum", "class_w_sql_1_1_w_sql_datum.html", null ],
+      [ "WSql::WSqlDriver", "class_w_sql_1_1_w_sql_driver.html", null ],
+      [ "WSql::WSqlDriverFactory", "class_w_sql_1_1_w_sql_driver_factory.html", null ],
+      [ "WSql::WSqlError", "class_w_sql_1_1_w_sql_error.html", null ],
+      [ "WSql::WSqlField", "class_w_sql_1_1_w_sql_field.html", null ],
+      [ "WSql::WSqlForeignKey", "class_w_sql_1_1_w_sql_foreign_key.html", null ],
+      [ "WSql::WSqliteDriver", "class_w_sql_1_1_w_sqlite_driver.html", null ],
+      [ "WSql::WSqliteResult", "class_w_sql_1_1_w_sqlite_result.html", null ],
+      [ "WSql::WSqlRecord", "class_w_sql_1_1_w_sql_record.html", null ],
+      [ "WSql::WSqlReferencedKey", "class_w_sql_1_1_w_sql_referenced_key.html", null ],
+      [ "WSql::WSqlResult", "class_w_sql_1_1_w_sql_result.html", null ],
+      [ "WSql::WSqlTable", "class_w_sql_1_1_w_sql_table.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "WSql::WormClassGenerator", "class_w_sql_1_1_worm_class_generator.html", null ],
+      [ "WSql::WormCodeTemplate", "class_w_sql_1_1_worm_code_template.html", null ],
+      [ "WSql::WSqlColumn", "class_w_sql_1_1_w_sql_column.html", null ],
+      [ "WSql::WSqlDabataseManager", "class_w_sql_1_1_w_sql_dabatase_manager.html", null ],
+      [ "WSql::WSqlDatabase", "class_w_sql_1_1_w_sql_database.html", null ],
+      [ "WSql::WSqlDatum", "class_w_sql_1_1_w_sql_datum.html", null ],
+      [ "WSql::WSqlDriver", "class_w_sql_1_1_w_sql_driver.html", [
+        [ "WSql::WMysqlDriver", "class_w_sql_1_1_w_mysql_driver.html", null ],
+        [ "WSql::WSqliteDriver", "class_w_sql_1_1_w_sqlite_driver.html", null ]
+      ] ],
+      [ "WSql::WSqlDriverFactory", "class_w_sql_1_1_w_sql_driver_factory.html", null ],
+      [ "WSql::WSqlError", "class_w_sql_1_1_w_sql_error.html", null ],
+      [ "WSql::WSqlField", "class_w_sql_1_1_w_sql_field.html", null ],
+      [ "WSql::WSqlForeignKey", "class_w_sql_1_1_w_sql_foreign_key.html", null ],
+      [ "WSql::WSqlRecord", "class_w_sql_1_1_w_sql_record.html", null ],
+      [ "WSql::WSqlReferencedKey", "class_w_sql_1_1_w_sql_referenced_key.html", null ],
+      [ "WSql::WSqlResult", "class_w_sql_1_1_w_sql_result.html", [
+        [ "WSql::WMysqlResult", "class_w_sql_1_1_w_mysql_result.html", null ],
+        [ "WSql::WSqliteResult", "class_w_sql_1_1_w_sqlite_result.html", null ]
+      ] ],
+      [ "WSql::WSqlTable", "class_w_sql_1_1_w_sql_table.html", null ]
+    ] ],
+    [ "Class Members", "functions.html", null ],
+    [ "Namespace List", "namespaces.html", [
+      [ "WORM", "namespace_w_o_r_m.html", null ],
+      [ "WSql", "namespace_w_sql.html", null ],
+      [ "WSql::WSqlDataType", "namespace_w_sql_1_1_w_sql_data_type.html", null ],
+      [ "WSqlDataType", "namespace_w_sql_data_type.html", null ]
+    ] ],
+    [ "Namespace Members", "namespacemembers.html", null ],
+    [ "File List", "files.html", [
+      [ "src/wormgen.cpp", "wormgen_8cpp.html", null ],
+      [ "src/orm/wormclassgenerator.cpp", "wormclassgenerator_8cpp.html", null ],
+      [ "src/orm/wormclassgenerator.h", "wormclassgenerator_8h.html", null ],
+      [ "src/orm/wormcodetemplate.cpp", "wormcodetemplate_8cpp.html", null ],
+      [ "src/orm/wormcodetemplate.h", "wormcodetemplate_8h.html", null ],
+      [ "src/orm/templates/class_declaration.tpl.varnames.h", "class__declaration_8tpl_8varnames_8h.html", null ],
+      [ "src/sql/wsql.h", "wsql_8h.html", null ],
+      [ "src/sql/wsqlcolumn.cpp", "wsqlcolumn_8cpp.html", null ],
+      [ "src/sql/wsqlcolumn.h", "wsqlcolumn_8h.html", null ],
+      [ "src/sql/wsqldabatasemanager.cpp", "wsqldabatasemanager_8cpp.html", null ],
+      [ "src/sql/wsqldabatasemanager.h", "wsqldabatasemanager_8h.html", null ],
+      [ "src/sql/wsqldatabase.cpp", "wsqldatabase_8cpp.html", null ],
+      [ "src/sql/wsqldatabase.h", "wsqldatabase_8h.html", null ],
+      [ "src/sql/wsqldatatype.h", "wsqldatatype_8h.html", null ],
+      [ "src/sql/wsqldatum.cpp", "wsqldatum_8cpp.html", null ],
+      [ "src/sql/wsqldatum.h", "wsqldatum_8h.html", null ],
+      [ "src/sql/wsqldriver.cpp", "wsqldriver_8cpp.html", null ],
+      [ "src/sql/wsqldriver.h", "wsqldriver_8h.html", null ],
+      [ "src/sql/wsqldriverfactory.cpp", "wsqldriverfactory_8cpp.html", null ],
+      [ "src/sql/wsqldriverfactory.h", "wsqldriverfactory_8h.html", null ],
+      [ "src/sql/wsqlerror.cpp", "wsqlerror_8cpp.html", null ],
+      [ "src/sql/wsqlerror.h", "wsqlerror_8h.html", null ],
+      [ "src/sql/wsqlfield.cpp", "wsqlfield_8cpp.html", null ],
+      [ "src/sql/wsqlfield.h", "wsqlfield_8h.html", null ],
+      [ "src/sql/wsqlforeignkey.cpp", "wsqlforeignkey_8cpp.html", null ],
+      [ "src/sql/wsqlforeignkey.h", "wsqlforeignkey_8h.html", null ],
+      [ "src/sql/wsqlrecord.cpp", "wsqlrecord_8cpp.html", null ],
+      [ "src/sql/wsqlrecord.h", "wsqlrecord_8h.html", null ],
+      [ "src/sql/wsqlreferencedkey.cpp", "wsqlreferencedkey_8cpp.html", null ],
+      [ "src/sql/wsqlreferencedkey.h", "wsqlreferencedkey_8h.html", null ],
+      [ "src/sql/wsqlresult.cpp", "wsqlresult_8cpp.html", null ],
+      [ "src/sql/wsqlresult.h", "wsqlresult_8h.html", null ],
+      [ "src/sql/wsqltable.cpp", "wsqltable_8cpp.html", null ],
+      [ "src/sql/wsqltable.h", "wsqltable_8h.html", null ],
+      [ "src/sql/drivers/wmysqldriver.cpp", "wmysqldriver_8cpp.html", null ],
+      [ "src/sql/drivers/wmysqldriver.h", "wmysqldriver_8h.html", null ],
+      [ "src/sql/drivers/wmysqlresult.cpp", "wmysqlresult_8cpp.html", null ],
+      [ "src/sql/drivers/wmysqlresult.h", "wmysqlresult_8h.html", null ],
+      [ "src/sql/drivers/wsqlitedriver.cpp", "wsqlitedriver_8cpp.html", null ],
+      [ "src/sql/drivers/wsqlitedriver.h", "wsqlitedriver_8h.html", null ],
+      [ "src/sql/drivers/wsqliteresult.cpp", "wsqliteresult_8cpp.html", null ],
+      [ "src/sql/drivers/wsqliteresult.h", "wsqliteresult_8h.html", null ]
+    ] ],
+    [ "File Members", "globals.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+

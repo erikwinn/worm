67e2e842f586e2e0867fbf13b8569349b85164ad
Erik Winn <erikwinnmail@yahoo.com>
2011-11-27 11:08:18 -0700

    changed email address

--- src/sql/drivers/wmysqldriver.cpp
+++ src/sql/drivers/wmysqldriver.cpp
@@ -1,6 +1,6 @@
 /*
     WORM - a DAL/ORM code generation framework
-    Copyright (C) 2011  Erik Winn <erikwinnmail@yahoo.com>
+    Copyright (C) 2011  Erik Winn <sidewalksoftware@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
